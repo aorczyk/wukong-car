7cb4c4c7f9b84f3e242893d5376cf2d57f9e68c0
Adam Orczyk <adam.orczyk@gmail.com>
2024-12-18 17:32:53 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -33,7 +33,7 @@ basic.forever(function () {
             bluetooth.uartWriteLine('vc;jry;-100;100;1;0;0;')
             bluetooth.uartWriteLine('vc;b;Digit1;1;0;<i class="fa-solid fa-volume-high"></i>;')
             bluetooth.uartWriteLine('vc;b;Digit2;0;0;2;')
-            bluetooth.uartWriteLine('vc;b;Digit3;1;0;M1;')
+            // bluetooth.uartWriteLine('vc;b;Digit3;1;0;M1;')
             bluetooth.uartWriteLine('vc;b;Digit4;0;0;4;')
             bluetooth.uartWriteLine('vc;ox;1;-30;30;-60;60;10;1;0;')
             bluetooth.uartWriteLine('vc;oy;0;-30;30;-100;100;10;0;0;')
@@ -41,6 +41,8 @@ basic.forever(function () {
             bluetooth.uartWriteLine('vc;ir;1;')
             bluetooth.uartWriteLine('vc;show;sl,sr,jr,br,bl;')
             bluetooth.uartWriteLine('vc;import_end;')
+            
+            bluetooth.uartWriteLine('vc;b;Digit3;1;0;M1;')
         } else if (commandName == "oy" || commandName == "sl" || commandName == "jry") {
             if (mode == 0) {
                 wuKong.setServoSpeed(wuKong.ServoList.S1, commandValue)
